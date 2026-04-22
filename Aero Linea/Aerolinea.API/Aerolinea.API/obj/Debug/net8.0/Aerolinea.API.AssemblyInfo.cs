da322bb3e186bfcb252c3048eadaee7843449a4e
MynitorBa <mynitorbarrios2006@gmail.com>
2026-04-21 20:00:05 -0600

    Commit errores

--- Aero Linea/Aerolinea.API/Aerolinea.API/obj/Debug/net8.0/Aerolinea.API.AssemblyInfo.cs
+++ Aero Linea/Aerolinea.API/Aerolinea.API/obj/Debug/net8.0/Aerolinea.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aerolinea.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc7a0b6f6736ca8dfd4d6c6c80939c73fc7889f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07affd786a7798aa8bfddb7ad43a08083cc32df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aerolinea.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aerolinea.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
